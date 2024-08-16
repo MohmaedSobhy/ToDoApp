eb3589b12f42b214bcb340c1604c2235f25097e5
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-08-16 16:07:56 +0300

    Task cubit bloc provider

--- lib/to_do_app.dart
+++ lib/to_do_app.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:to_do_app/Core/routes/app_route.dart';
 import 'package:to_do_app/Core/style/theme/app_theme.dart';
+import 'package:to_do_app/Feature/home/presentation/controller/task_cubit.dart';
 
 class ToDoApp extends StatelessWidget {
   const ToDoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.appTheme,
-      initialRoute: AppRoutes.splashScreen,
-      onGenerateRoute: AppRoutes.onGenerateRoute,
-      debugShowCheckedModeBanner: false,
+    return BlocProvider.value(
+      value: TaskCubit.instances,
+      child: MaterialApp(
+        theme: AppTheme.appTheme,
+        initialRoute: AppRoutes.splashScreen,
+        onGenerateRoute: AppRoutes.onGenerateRoute,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
