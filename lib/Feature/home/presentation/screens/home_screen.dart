06686f00b312d05037a1847423156afe97e3e5e0
Mohamed sobhy <mohamedyamany@055@gmail.com>
2024-08-17 16:38:52 +0300

    navigate to add task screen

--- lib/Feature/home/presentation/screens/home_screen.dart
+++ lib/Feature/home/presentation/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/Core/routes/app_route.dart';
-import 'package:to_do_app/Core/services/column_key.dart';
-import 'package:to_do_app/Core/services/sqlite_services.dart';
 import 'package:to_do_app/Feature/home/presentation/views/home_body_view.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,16 +11,7 @@ class HomeScreen extends StatelessWidget {
       body: const SafeArea(child: HomeBodyView()),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          // Navigator.of(context).pushNamed(AppRoutes.addTask);
-          await AppData.insertDB({
-            ColumnKey.title: 'Hello World',
-            ColumnKey.note: 'Hello World',
-            ColumnKey.startTime: 'Hello World',
-            ColumnKey.endTime: 'Hello World',
-            ColumnKey.date: 'Hello World',
-          }).then((value) {
-            print(value);
-          });
+          Navigator.of(context).pushNamed(AppRoutes.addTask);
         },
         shape: const CircleBorder(),
         child: const Icon(Icons.add),
